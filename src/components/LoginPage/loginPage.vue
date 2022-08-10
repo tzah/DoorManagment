3c0195511a5508253fa29e7b7be93612372cd709
tzahinakash <tzahinakash2000@gmail.com>
2022-08-10 16:01:43 +0300

    LoginPage background

---
+++ src/components/LoginPage/loginPage.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <img
+      src="../../assets/background/big_circle_right.svg"
+      class="big_circle_right"
+    />
+    <img
+      src="../../assets/background/big_circle_left.svg"
+      class="big_circle_left"
+    />
+    <div>
+      <img
+        src="../../assets/background/mid_circle_right.svg"
+        class="mid_circle_right"
+      />
+      <img
+        src="../../assets/background/small_circle_right.svg"
+        class="small_circle_right"
+      />
+      <img src="../../assets/background/small_top.svg" class="small_top" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "loginPage",
+};
+</script>
+
+<style scoped>
+.big_circle_right {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+
+.big_circle_left {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.mid_circle_right {
+  position: absolute;
+  right: 0;
+  bottom: 45vh;
+}
+
+.small_circle_right {
+  position: absolute;
+  right: 10vw;
+  bottom: 55vh;
+}
+
+.small_top {
+  position: absolute;
+  right: 13vw;
+  bottom: 38vh;
+}
+</style>
