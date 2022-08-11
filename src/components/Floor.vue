865062e3541b88bfbc5f00711884d09345b3581f
lior3308 <liora,mualem@gmail.com>
2022-08-11 11:54:46 +0300

    added room floor and mainpage

---
+++ src/components/Floor.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="fullPage">
+        <div class="rooms-row">
+            <div  v-for="room in getRoomsByDirection('bottom')" :key="room.name">
+                <Room 
+                  :isOpen="room.isOpen"
+                  :name="room.name"
+                  :doorDirection="room.doorDirection"/>
+            </div>
+        </div>
+        <br/>
+        <div class="rooms-row" >
+            <div v-for="room in getRoomsByDirection('top')" :key="room.name">
+                <Room 
+                  :isOpen="room.isOpen"
+                  :name="room.name"
+                  :doorDirection="room.doorDirection"/>
+            </div>
+        </div>
+    </div>
+    
+</template>
+<script>
+import Room from './Room.vue'
+
+export default {
+    name: 'Floor',
+
+    data: () => ({
+    }),
+    components: {
+        Room,
+    },
+    props: {
+        rooms: {
+            type: Array(),
+            default: [],
+        },
+    },
+    methods: {
+        getRoomsByDirection(direction) {
+            console.log(direction, this.rooms, this.rooms.filter(room => room.doorDirection === direction))
+            return this.rooms.filter(room => room.doorDirection === direction);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.rooms-row {
+    display: flex;
+}
+
+.fullPage {
+    height: 100%;
+    width: 100%;
+}
+</style>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="container">
+        <Floor :rooms="rooms"/>
+    </div>
+</template>
+<script>
+import Floor from '../components/Floor.vue'
+
+export default {
+    name: 'MainPage',
+
+    data: () => ({
+        rooms: [{
+            isOpen: true,
+            name: "door 1",
+            doorDirection: "top",
+        }, {
+            isOpen: true,
+            name: "door 2",
+            direcdoorDirectiontion: "bottom",
+        }, {
+            isOpen: false,
+            name: "door 3",
+            doorDirection: "top",
+        }, {
+            isOpen: false,
+            name: "door 4",
+            doorDirection: "top",
+        }, {
+            isOpen: true,
+            name: "door 5",
+            doorDirection: "bottom",
+        }, {
+            isOpen: true,
+            name: "door 6",
+            doorDirection: "bottom",
+        }, {
+            isOpen: false,
+            name: "door 7",
+            doorDirection: "top",
+        }, {
+            isOpen: true,
+            name: "door 8",
+            doorDirection: "bottom",
+        }, {
+            isOpen: true,
+            name: "door 9",
+            doorDirection: "top",
+        }, {
+            isOpen: false,
+            name: "door 10",
+            doorDirection: "top",
+        }, {
+            isOpen: true,
+            name: "door 11",
+            doorDirection: "bottom",
+        }, {
+            isOpen: false,
+            name: "door 12",
+            doorDirection: "bottom",
+        }, {
+            isOpen: true,
+            name: "door 13",
+            doorDirection: "bottom",
+        }, {
+            isOpen: false,
+            name: "door 12",
+            doorDirection: "top",
+        }, {
+            isOpen: true,
+            name: "door 13",
+            doorDirection: "bottom",
+        }
+        ]
+    }),
+    components: {
+        Floor,
+    }
+}
+</script>
+
+<style scoped>
+.container {
+    padding: 25vh;
+
+    justify-content: center;
+}
+</style>
